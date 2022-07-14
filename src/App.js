71d54dc83ae0852984f0aa645e5110e3e2b82b31
Đoàn Xuân Bách <50036032+DarkDrake132@users.noreply.github.com>
2022-07-14 22:02:06 +0700

    Add Host and Game components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import { Route, BrowserRouter } from "react-router-dom";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
 import Landing from "./components/Landing/Landing";
-// import Main from "./components/Host/Main";
-// import New_Quiz from "./components/Host/New_Quiz";
-// import Game from "./components/Game/Game";
-// import Questions from "./components/Host/Questions";
-// import New_Question from "./components/Host/New_Question";
-// import Player from "./components/Game/Player";
-// import Edit_Question from "./components/Host/Edit_Question";
+import Main from "./components/Host/Main";
+import New_Quiz from "./components/Host/New_Quiz";
+import Game from "./components/Game/Game";
+import Questions from "./components/Host/Questions";
+import New_Question from "./components/Host/New_Question";
+import Player from "./components/Game/Player";
+import Edit_Question from "./components/Host/Edit_Question";
 import "./App.css";
 import "./reset.css";
 
@@ -16,14 +16,16 @@ class App extends Component {
     return (
       <div>
         <BrowserRouter>
-          <Route path="/" exact component={Landing} />
-          {/* <Route path="/game" component={Game} />
-          <Route path="/host" exact component={Main} />
-          <Route path="/host/newQuiz" component={New_Quiz} />
-          <Route path="/host/questions" component={Questions} />
-          <Route path="/host/newquestion/:id" component={New_Question} />
-          <Route path="/host/editquestion/:id" component={Edit_Question} />
-          <Route path="/player" component={Player} /> */}
+          <Switch>
+            <Route path="/" exact component={Landing} />
+            <Route path="/game" component={Game} />
+            <Route path="/host" exact component={Main} />
+            <Route path="/host/newQuiz" component={New_Quiz} />
+            <Route path="/host/questions" component={Questions} />
+            <Route path="/host/newquestion/:id" component={New_Question} />
+            <Route path="/host/editquestion/:id" component={Edit_Question} />
+            <Route path="/player" component={Player} />
+          </Switch>
         </BrowserRouter>
       </div>
     );
